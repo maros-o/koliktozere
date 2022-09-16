5c266d8c88e1619d7febcb7b4872d589b949e45e
maros-o <maros.mec@seznam.cz>
2022-09-16 16:20:18 +0200

    tailwind

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,7 +4,7 @@ import ResultsChart from "./ResultsChart";
 export default function Results({ results }) {
   return (
     <div>
-      <div className="text-xl md:text-2xl m-2 mt-5 mb-4 text-center drop-shadow-2xl px-4">
+      <div className="text-xl md:text-2xl m-2 mt-5 mb-4 text-center drop-shadow-2xl px-3">
         Za elektřinu měsíčně zaplatíte přibližně {Math.round(results.totalCost)}{" "}
         Kč
       </div>
